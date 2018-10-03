921fdbbf7d3c9029c4af2e54e8107cb2615d224f
Felix Rilling <felix@f-rilling.com>
2018-10-03 09:50:22 +0200

    Improved default appenderFn.

--- src/appender/defaultAppenderFn.ts
+++ src/appender/defaultAppenderFn.ts
@@ -1,10 +1,35 @@
 import { ILevel } from "../level/ILevel";
+import { Levels } from "../level/Levels";
 import { appenderFn } from "./appenderFn";
 
+/**
+ * The default appender-fn, doing the actual logging.
+ *
+ * @private
+ * @param level Level of the entry to log.
+ * @param name Name of the logger instance.
+ * @param args Arguments to log.
+ */
 const defaultAppenderFn: appenderFn = (
     level: ILevel,
     name: string,
     args: any[]
-) => console.log(`${new Date().toISOString()} ${level.name} ${name}`, ...args);
+) => {
+    const meta = `${new Date().toISOString()} ${level.name} ${name}`;
+    let loggerFn = console.log;
+
+    if (level === Levels.ERROR) {
+        // tslint:disable-next-line
+        loggerFn = console.error;
+    } else if (level === Levels.WARN) {
+        // tslint:disable-next-line
+        loggerFn = console.warn;
+    } else if (level === Levels.INFO) {
+        // tslint:disable-next-line
+        loggerFn = console.info;
+    }
+
+    loggerFn(meta, ...args);
+};
 
 export { defaultAppenderFn };
--- types/appender/defaultAppenderFn.d.ts
+++ types/appender/defaultAppenderFn.d.ts
@@ -1,3 +1,11 @@
 import { appenderFn } from "./appenderFn";
+/**
+ * The default appender-fn, doing the actual logging.
+ *
+ * @private
+ * @param level Level of the entry to log.
+ * @param name Name of the logger instance.
+ * @param args Arguments to log.
+ */
 declare const defaultAppenderFn: appenderFn;
 export { defaultAppenderFn };
